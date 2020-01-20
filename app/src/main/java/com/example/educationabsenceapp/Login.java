727d9a044c30f9558a3450f6575e0beb2f63bda4
JIBaker92 <j_i_baker@yahoo.co.uk>
2020-01-20 23:35:10 +0000

    Seventh Commit, Login page coded to allow registered users to login to the app and TextView allows for movement between Login and Register activities

--- app/src/main/java/com/example/educationabsenceapp/Login.java
+++ app/src/main/java/com/example/educationabsenceapp/Login.java
@@ -20,11 +20,64 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
 
+    EditText mEmail,mPassword;
+    Button mLoginBtn;
+    TextView mCreateBtn;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmail      = findViewById(R.id.Email);
+        mPassword   = findViewById(R.id.Password);
+        mLoginBtn   = findViewById(R.id.login_btn);
+        mCreateBtn  = findViewById(R.id.reg_txt);
+        fAuth       = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email    = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                //Validate email and password
+                if (TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is required.");
+                    return;
+                }
+
+                if (password.length() < 6){
+                    mPassword.setError("Password must be more than six characters in length.");
+                    return;
+                }
+
+                progressBar.setVisibility(View.GONE);
+
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(Login.this, "Login successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+                        }else{
+
+                            Toast.makeText(Login.this, "Error " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
 
     }
 }
