b7bff1dab57187525c1dfe81bb9de0895e91eeb0
JIBaker92 <j_i_baker@yahoo.co.uk>
2020-01-27 20:56:08 +0000

    13th commit the user once logged in can now send an email with intent

--- app/src/main/java/com/example/educationabsenceapp/MainActivity.java
+++ app/src/main/java/com/example/educationabsenceapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
     TextView email_not_verified_txt;
     Button verify_btn;
     FirebaseAuth fAuth;
-
+    private EditText mrecipient, msubject, mmessage;
+    Button send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
         verify_btn = findViewById(R.id.verify_btn);
         email_not_verified_txt = findViewById(R.id.email_not_verified_txt);
         FirebaseUser user = fAuth.getCurrentUser();
+        mrecipient = findViewById(R.id.recipient_txt);
+        msubject = findViewById(R.id.subject_txt);
+        mmessage = findViewById(R.id.message_txt);
+        send = findViewById(R.id.send_btn);
+
 
         if (!user.isEmailVerified()){
             email_not_verified_txt.setVisibility(View.VISIBLE);
@@ -57,8 +64,30 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMail();
+            }
+        });
 
+    }
 
+    private void sendMail(){
+        String recipientList = mrecipient.getText().toString();
+        String[] recipients = recipientList.split(",");
+        //allows for more than one email address and then splits them email 1, email 2
+
+        String subject = msubject.getText().toString();
+        String message = mmessage.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+
+        intent.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent, "choose an email client"));
     }
 
     public void logout(View view) {
